702508560a5f66c3592f61d56cd4745dcd59283f
miji <mijilee0413@sookmyung.ac.kr>
2023-10-08 11:27:30 +0900

    4-2
    사칙연산
    -입력값이 정수가 아닐때 에러 표시
    -분모가 0인 경우 에러 처리

--- 4-2main.c
+++ 4-2main.c
@@ -1,22 +1,31 @@
+//사칙연산 계산기
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
-	unsigned int x;
-	int b=0;
+	int a, b;
 	
-	printf("Input a number : ");
-	scanf("%ui", &x);
+	printf("Input two integers : ");
 	
-	for (b=0; x !=0; x>>=1)
-	{
-		if (x & 1)
-		{
-			b++;
-		}
+	// 입력값이 정수가 아닐때 에러 처리
+	if (scanf("%i %i", &a, &b) != 2) {
+        printf("Invalid input. Please enter two integers.\n");
+        return EXIT_FAILURE;
+    }
+	
+	printf("%i + %i = %i \n", a, b, a+b);
+	printf("%i - %i = %i \n", a, b, a-b);
+	printf("%i * %i = %i \n", a, b, a*b);
+	
+	// 분모가 0인 경우 에러 처리
+	if (b != 0) {
+		printf("%i / %i = %i \n", a, b, a/b);
+		printf("%i %% %i = %i \n", a, b, a%b);
+	} else {
+		printf("Division by zero is not allowed.\n");
 	}
 	
-	printf("The result is : %i\n", b);
+	system("PAUSE");
 	return 0;
 }
