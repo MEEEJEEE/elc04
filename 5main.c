036637925b8aaf7fbb0197354abcae4615b140a8
miji <mijilee0413@sookmyung.ac.kr>
2023-10-08 11:39:00 +0900

    4-5
    bit상에서 1의 개수 계산

---
+++ 5main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	unsigned int x;
+	int b = 0; // 변수 b를 초기화
+
+	printf("Input a number : ");
+	scanf("%ui", &x);
+
+	for (b=0; x !=0; x>>=1)
+	{
+		if (x & 1)
+		{
+			b++;
+		}
+	}
+
+	printf("The result is : %i\n", b);
+	return 0;
+}
